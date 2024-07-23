48b521f0caab5dc8a18f3c90ac4224d6923b357f
JuliaOliveira26 <julinhalves2006@gmail.com>
2024-07-23 11:09:54 -0300

    Ajustes_elementos_garagem, env,layout telas

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <script setup>
-  import { useMonitor } from '@/composables/monitor';
+//import { onMounted } from 'vue';
+//import { PassageUser } from '@passageidentity/passage-elements/passage-user';
+import { useAuthStore } from '@/stores/auth';
 
-  const { menu } = useMonitor();
+const authStore = useAuthStore();
+
 </script>
 
+
 <template>
-  <div>
-    <component :is="menu" />
-    <main>
-      <router-view />
-    </main>
-    <footer>
-      <p>Copyright &copy; 2024</p>
-    </footer>
-  </div>
+  <header>
+    <router-link :to="{ name: 'home' }">Home</router-link> |
+    <router-link to="/categorias">Categorias</router-link> |
+    <router-link to="/acessorios">Acessórios</router-link> |
+    <router-link to="/marcas">Marcas</router-link> |
+    <router-link to="/cores">Cores</router-link> |
+    <div v-if="authStore.loggedIn">
+    <router-link  to="/logout">Logout</router-link> |
+      {{ authStore.user.email }}
+    </div>
+    <router-link v-else to="/login">Login</router-link>
+  </header>
+  <main>
+    <RouterView />
+  </main>
 </template>
